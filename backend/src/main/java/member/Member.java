f165f6578c823d1a0f74901ccbcb5ac7a40e0f3a
임준혁 <junhyuk1027@naver.com>
2024-09-03 17:58:51 +0900

    Feat: member entity column add, package name changed

--- backend/src/main/java/member/Member.java
+++ backend/src/main/java/member/Member.java
@@ -1,5 +1,6 @@
-package entity;
+package member;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
@@ -10,5 +11,8 @@ public class Member {
     @Id
     @GeneratedValue
     private int id;
+
+    @Column
+    private String name;
 
 }
