7332da93e5ddd3c83e44dabe5b14def7ff3b2d28
임준혁 <junhyuk1027@naver.com>
2024-09-04 13:03:49 +0900

    Feat: kafka producer created

---
+++ outbox-kafka-producer/src/main/java/com/ssafy/a303/outbox_kafka_producer/OutboxKafkaProducerApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.a303.outbox_kafka_producer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OutboxKafkaProducerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OutboxKafkaProducerApplication.class, args);
+	}
+
+}
---
+++ outbox-kafka-producer/src/test/java/com/ssafy/a303/outbox_kafka_producer/OutboxKafkaProducerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.a303.outbox_kafka_producer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OutboxKafkaProducerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
