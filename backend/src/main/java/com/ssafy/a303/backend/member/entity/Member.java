0eb12b8354051867f9dc18b62d214e2b6aa86822
박동환 <znxn123@naver.com>
2024-09-06 12:43:27 +0900

    Feat: Update Member entity
    Add Getter, NoAgrsConstructor Annotation, reports, isDeleted variable
    Member mapped to Report entity

--- backend/src/main/java/com/ssafy/a303/backend/member/entity/Member.java
+++ backend/src/main/java/com/ssafy/a303/backend/member/entity/Member.java
@@ -1,16 +1,28 @@
 package com.ssafy.a303.backend.member.entity;
 
+import com.ssafy.a303.backend.report.entity.Report;
 import jakarta.persistence.Column;
 import jakarta.persistence.Embedded;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import java.util.ArrayList;
+import java.util.List;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.ColumnDefault;
 
+@Getter
 @Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Member {
 
     @Id
     @GeneratedValue
+    @Column(name = "MEMBER_ID")
     private int id;
 
     @Column(nullable = false)
@@ -24,4 +36,20 @@ public class Member {
 
     @Embedded
     private Address address;
+
+    @OneToMany(mappedBy = "member")
+    List<Report> reports = new ArrayList<>();
+
+    @ColumnDefault("false")
+    private boolean isDeleted;
+
+    @Builder
+    public Member(String email, String password, String name, String tel, Address address) {
+        this.email = email;
+        this.password = password;
+        this.name = name;
+        this.tel = tel;
+        this.address = address;
+    }
+
 }
