57eb01e6b749e6765dc385286c15b7c8cfa372ea
변지환 <jihwaja9091@naver.com>
2024-09-10 17:58:54 +0900

    Feat: add Repository annotation

--- backend/src/main/java/com/ssafy/a303/backend/domain/report/repository/ReportRepository.java
+++ backend/src/main/java/com/ssafy/a303/backend/domain/report/repository/ReportRepository.java
@@ -1,8 +1,11 @@
 package com.ssafy.a303.backend.domain.report.repository;
 
-import com.ssafy.a303.backend.report.entity.Report;
-import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.ssafy.a303.backend.domain.report.entity.Report;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
 public interface ReportRepository extends JpaRepository<Report, Long> {
 
 }
