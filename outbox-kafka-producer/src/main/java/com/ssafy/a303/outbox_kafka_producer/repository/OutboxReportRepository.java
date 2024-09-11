b452d21db1a3b5685d3a8ebd499d47ba87dc38c2
임준혁 <junhyuk1027@naver.com>
2024-09-11 17:58:41 +0900

    Feat: repository extends jpa repo

--- outbox-kafka-producer/src/main/java/com/ssafy/a303/outbox_kafka_producer/repository/OutboxReportRepository.java
+++ outbox-kafka-producer/src/main/java/com/ssafy/a303/outbox_kafka_producer/repository/OutboxReportRepository.java
@@ -1,8 +1,10 @@
 package com.ssafy.a303.outbox_kafka_producer.repository;
 
+import com.ssafy.a303.outbox_kafka_producer.entity.OutboxReport;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public class OutboxReportRepository {
+public interface OutboxReportRepository extends JpaRepository<OutboxReport, Integer> {
     
 }
