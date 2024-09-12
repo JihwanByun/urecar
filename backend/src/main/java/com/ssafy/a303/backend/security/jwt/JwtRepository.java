e3036756dd8da2f1f9718d4e7b659253416698ee
박동환 <znxn123@naver.com>
2024-09-12 14:01:07 +0900

    Fix(JwtRepository): fix Annotation error

--- backend/src/main/java/com/ssafy/a303/backend/security/jwt/JwtRepository.java
+++ backend/src/main/java/com/ssafy/a303/backend/security/jwt/JwtRepository.java
@@ -2,7 +2,6 @@ package com.ssafy.a303.backend.security.jwt;
 
 import java.time.Duration;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.stereotype.Repository;
@@ -13,7 +12,7 @@ public class JwtRepository {
 
     private final RedisTemplate<String, Object> redisTemplate;
 
-    public JwtRepository(@Qualifier("redisTemplate") RedisTemplate<String, Object> redisTemplate) {
+    public JwtRepository(RedisTemplate<String, Object> redisTemplate) {
         this.redisTemplate = redisTemplate;
     }
 
