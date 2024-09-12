d2e75c067b56036acedfd12764b1b541f2b029d2
박동환 <znxn123@naver.com>
2024-09-12 13:49:16 +0900

    Feat: add jwt access denied handler

---
+++ backend/src/main/java/com/ssafy/a303/backend/security/handler/JwtAccessDeniedHandler.java
@@ -0,0 +1,29 @@
+package com.ssafy.a303.backend.security.handler;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.ssafy.a303.backend.exception.ErrorCode;
+import com.ssafy.a303.backend.exception.ErrorResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.web.access.AccessDeniedHandler;
+import org.springframework.stereotype.Component;
+import java.io.IOException;
+
+@Slf4j
+@Component
+public class JwtAccessDeniedHandler implements AccessDeniedHandler {
+
+    ObjectMapper objectMapper = new ObjectMapper();
+
+    @Override
+    public void handle(HttpServletRequest request, HttpServletResponse response, AccessDeniedException accessDeniedException) throws IOException {
+        log.info("[Authorization 실패]");
+        response.setHeader("Content-type","application/json");
+        response.setCharacterEncoding("UTF-8");
+        response.setStatus(ErrorCode.UNAUTHORIZED_ACCESS.getStatus());
+        response.getWriter().write(objectMapper.writeValueAsString(new ErrorResponse(ErrorCode.UNAUTHORIZED_ACCESS)));
+    }
+
+}
