dcc29d685160d7e68c1751e89451fda50b8954aa
박동환 <znxn123@naver.com>
2024-09-12 13:52:07 +0900

    Feat: create jwtRepository

--- backend/src/main/java/com/ssafy/a303/backend/security/jwt/JwtRepository.java
+++ backend/src/main/java/com/ssafy/a303/backend/security/jwt/JwtRepository.java
@@ -1,5 +1,39 @@
 package com.ssafy.a303.backend.security.jwt;
 
+import java.time.Duration;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
+import org.springframework.stereotype.Repository;
+
+@Slf4j
+@Repository
 public class JwtRepository {
 
+    private final RedisTemplate<String, Object> redisTemplate;
+
+    public JwtRepository(@Qualifier("redisTemplate") RedisTemplate<String, Object> redisTemplate) {
+        this.redisTemplate = redisTemplate;
+    }
+
+    public void save(String key, String value, long expirationMinutes){
+        ValueOperations<String, Object> values = redisTemplate.opsForValue();
+        values.set(key, value, Duration.ofSeconds(expirationMinutes));
+    }
+
+    public Object findByKey(Object key) {
+        ValueOperations<String, Object> valueOperations = redisTemplate.opsForValue();
+        return valueOperations.get(key);
+    }
+
+    public boolean hasKey(String key) {
+        return Boolean.TRUE.equals(redisTemplate.hasKey(key));
+    }
+
+    public void delete(String key) {
+        redisTemplate.delete(key);
+    }
+
 }
+
