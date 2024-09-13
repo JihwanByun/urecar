fb239d2a59ad73eaf65c58dd9c0311079a299e0e
권대형 <doto3852@naver.com>
2024-09-13 15:46:46 +0900

    create login screen

--- frontend/lib/screens/login_screen.dart
+++ frontend/lib/screens/login_screen.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:frontend/components/common/button.dart';
 import 'package:frontend/components/common/input.dart';
 import 'package:frontend/components/common/input_label.dart';
+import 'package:frontend/screens/signup_screen.dart';
+import 'package:get/get.dart';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
@@ -34,6 +37,43 @@ class _LoginScreenState extends State<LoginScreen> {
             const Input(),
             const InputLabel(name: "비밀번호"),
             const Input(),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                TextButton(
+                  onPressed: () {
+                    Get.to(const SignupScreen());
+                  },
+                  child: const Text(
+                    "회원가입",
+                    style: TextStyle(
+                      color: Colors.black,
+                    ),
+                  ),
+                ),
+                const Text("ㅣ"),
+                TextButton(
+                  onPressed: () {
+                    Get.to(const SignupScreen());
+                  },
+                  child: const Text(
+                    "ID/PW 찾기",
+                    style: TextStyle(
+                      color: Colors.black,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 60),
+              child: Button(
+                  text: "로그인",
+                  onPressed: () {},
+                  horizontal: 100,
+                  vertical: 10,
+                  fontSize: 15),
+            )
           ],
         ),
       ),
