4fbdebb7bd79d9a05043bc5ac61054f1e1416482
박동환 <znxn123@naver.com>
2024-09-24 12:27:58 +0900

    Feat: remove Data annotation

--- backend/src/main/java/com/ssafy/a303/backend/domain/report/dto/CreateReportRequestDto.java
+++ backend/src/main/java/com/ssafy/a303/backend/domain/report/dto/CreateReportRequestDto.java
@@ -1,10 +1,8 @@
 package com.ssafy.a303.backend.domain.report.dto;
 
-import lombok.Data;
 import lombok.Getter;
 
 @Getter
-@Data
 public class CreateReportRequestDto {
 
     private Integer memberId;
