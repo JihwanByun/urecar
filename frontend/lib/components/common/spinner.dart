812df68785336cf6557d7d84a6e8a62c0559de75
김혁 <kimh0716@gmail.com>
2024-09-25 13:11:15 +0900

    feat: input spinner between camera_screen and check_image_screen

--- frontend/lib/components/common/spinner.dart
+++ frontend/lib/components/common/spinner.dart
@@ -2,25 +2,26 @@ import 'package:flutter/material.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
 
 class Spinner extends StatelessWidget {
-  // 미리 설정된 값
   final Color color;
   final double size;
   final Duration duration;
+  final Duration delay;
 
-  const Spinner({
-    super.key,
-    this.color = Colors.greenAccent, // 기본 색상
-    this.size = 50.0, // 기본 크기
-    this.duration = const Duration(milliseconds: 1200), // 기본 지속 시간
-  });
+  const Spinner(
+      {super.key,
+      this.color = Colors.greenAccent,
+      this.size = 50.0,
+      this.duration = const Duration(milliseconds: 1200),
+      this.delay = const Duration(milliseconds: 50)});
 
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: SpinKitRotatingCircle(
+      child: SpinKitThreeInOut(
         color: color,
         size: size,
         duration: duration,
+        delay: delay,
       ),
     );
   }
