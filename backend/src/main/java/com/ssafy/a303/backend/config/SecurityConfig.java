89617f9a9aa62e85d1facd7a52f376e234cc3ab6
박동환 <znxn123@naver.com>
2024-09-25 10:47:29 +0900

    Fix: Security Auth Error

--- backend/src/main/java/com/ssafy/a303/backend/config/SecurityConfig.java
+++ backend/src/main/java/com/ssafy/a303/backend/config/SecurityConfig.java
@@ -1,20 +1,20 @@
 package com.ssafy.a303.backend.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.ssafy.a303.backend.domain.member.repository.MemberRepository;
-import com.ssafy.a303.backend.domain.member.service.MemberServiceImpl;
+import com.ssafy.a303.backend.domain.member.service.MemberService;
 import com.ssafy.a303.backend.security.filter.JsonUsernamePasswordAuthenticationFilter;
 import com.ssafy.a303.backend.security.filter.JwtAuthenticationFilter;
 import com.ssafy.a303.backend.security.filter.JwtExceptionFilter;
 import com.ssafy.a303.backend.security.handler.JwtAccessDeniedHandler;
 import com.ssafy.a303.backend.security.handler.LoginFailureHandler;
 import com.ssafy.a303.backend.security.handler.LoginSuccessHandler;
-import com.ssafy.a303.backend.security.jwt.JwtRepository;
 import com.ssafy.a303.backend.security.jwt.JwtService;
 import com.ssafy.a303.backend.security.jwt.JwtUtil;
 import com.ssafy.a303.backend.security.user.AuthenticationProviderImpl;
 import com.ssafy.a303.backend.security.user.UserDetailsServiceImpl;
 import java.util.Arrays;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -23,54 +23,69 @@ import org.springframework.security.authentication.ProviderManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
+@Slf4j
 @Configuration
 @EnableWebSecurity
-public class SecurityConfig {
+@RequiredArgsConstructor
+public class SecurityConfig  {
+
+    private final ObjectMapper objectMapper;
+    private final UserDetailsServiceImpl userDetailsService;
+    private final AuthenticationProviderImpl authenticationProvider;
+    private final LoginSuccessHandler loginSuccessHandler;
+    private final LoginFailureHandler loginFailureHandler;
+    private final JwtAccessDeniedHandler jwtAccessDeniedHandler;
+    private final JwtExceptionFilter jwtExceptionFilter;
 
     @Bean
-    public SecurityFilterChain configure(HttpSecurity http, JwtRepository jwtRepository, MemberRepository memberRepository)
-            throws Exception {
+    public SecurityFilterChain filterChain(HttpSecurity http, MemberService memberService, JwtService jwtService) throws Exception {
 
         http
                 .csrf(AbstractHttpConfigurer::disable)
                 .cors((cors) -> cors.configurationSource(corsConfigurationSource()))
                 .httpBasic(AbstractHttpConfigurer::disable)
-                .formLogin(AbstractHttpConfigurer::disable)
-                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+                .sessionManagement(session -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
+        http
+                .formLogin(config -> config
+                        .successHandler(loginSuccessHandler));
 
         http
                 .authorizeHttpRequests(
                         authorize -> authorize
-                                .requestMatchers("/swagger", "/swagger-ui.html", "/swagger-ui/*", "/api-docs", "/api-docs/*",
-                                        "/v3/api-docs/*").permitAll()
+                                .requestMatchers("/swagger", "/swagger-ui.html", "/swagger-ui/*", "/api-docs", "/api-docs/*", "/v3/api-docs/*").permitAll()
                                 .requestMatchers(HttpMethod.POST, "/login", "/signup").permitAll()
-                                .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
+                                .requestMatchers(HttpMethod.OPTIONS,"/**").permitAll()
                                 .anyRequest().permitAll()
                 );
 
         http
-                .addFilterBefore(jsonUsernamePasswordAuthenticationFilter(jwtRepository, memberRepository), UsernamePasswordAuthenticationFilter.class)
-                .addFilterBefore(jwtAuthenticationFilter(jwtRepository, memberRepository), UsernamePasswordAuthenticationFilter.class)
-                .addFilterBefore(new JwtExceptionFilter(), JwtAuthenticationFilter.class);
+                .addFilterBefore(jsonUsernamePasswordAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class)
+                .addFilterBefore(new JwtAuthenticationFilter(memberService, jwtService, userDetailsService), UsernamePasswordAuthenticationFilter.class)
+                .addFilterBefore(jwtExceptionFilter, JwtAuthenticationFilter.class);
 
         http
                 .exceptionHandling((exceptionHandling) ->
-                        exceptionHandling.accessDeniedHandler(new JwtAccessDeniedHandler())
+                        exceptionHandling.accessDeniedHandler(jwtAccessDeniedHandler)
                 );
 
         http
-                .logout((logout) -> logout.permitAll().logoutSuccessHandler(logoutSuccessHandler(jwtRepository)));
+                .logout((logout) -> logout
+                        .permitAll()
+                        .logoutSuccessHandler(((request, response, authentication) -> {
+                            jwtService.saveLogoutAccessToken(request.getHeader("Authorization"));
+                            jwtService.deleteRefreshToken(JwtUtil.getLoginEmail(request.getHeader("Authorization")));
+                        }))
+                );
 
         return http.build();
     }
@@ -88,43 +103,22 @@ public class SecurityConfig {
     }
 
     @Bean
-    public JsonUsernamePasswordAuthenticationFilter jsonUsernamePasswordAuthenticationFilter(JwtRepository jwtRepository, MemberRepository memberRepository) {
+    public JsonUsernamePasswordAuthenticationFilter jsonUsernamePasswordAuthenticationFilter() {
         JsonUsernamePasswordAuthenticationFilter jsonUsernamePasswordAuthenticationFilter
-                = new JsonUsernamePasswordAuthenticationFilter(
-                new ObjectMapper(), new AuthenticationProviderImpl(new UserDetailsServiceImpl(memberRepository), passwordEncoder()));
-        jsonUsernamePasswordAuthenticationFilter.setAuthenticationSuccessHandler(new LoginSuccessHandler(new JwtService(jwtRepository)
-                , new MemberServiceImpl(memberRepository, passwordEncoder())));
-        jsonUsernamePasswordAuthenticationFilter.setAuthenticationFailureHandler(new LoginFailureHandler());
-        jsonUsernamePasswordAuthenticationFilter.setAuthenticationManager(authenticationManager(memberRepository));
+                = new JsonUsernamePasswordAuthenticationFilter(objectMapper, authenticationProvider);
+        jsonUsernamePasswordAuthenticationFilter.setAuthenticationManager(authenticationManager());
+        jsonUsernamePasswordAuthenticationFilter.setAuthenticationSuccessHandler(loginSuccessHandler);
+        jsonUsernamePasswordAuthenticationFilter.setAuthenticationFailureHandler(loginFailureHandler);
+
         return jsonUsernamePasswordAuthenticationFilter;
     }
 
     @Bean
-    public AuthenticationManager authenticationManager(MemberRepository memberRepository) {
+    public AuthenticationManager authenticationManager() {
         DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
-        provider.setUserDetailsService(new UserDetailsServiceImpl(memberRepository));
-        provider.setPasswordEncoder(passwordEncoder());
+        provider.setUserDetailsService(userDetailsService);
+        provider.setPasswordEncoder(new PasswordEncoderConfig().passwordEncoder());
         return new ProviderManager(provider);
-    }
-
-    @Bean
-    public JwtAuthenticationFilter jwtAuthenticationFilter(JwtRepository jwtRepository, MemberRepository memberRepository) {
-        return new JwtAuthenticationFilter(new MemberServiceImpl(memberRepository, passwordEncoder()), new JwtService(jwtRepository),
-                new UserDetailsServiceImpl(memberRepository));
-    }
-
-    @Bean
-    public LogoutSuccessHandler logoutSuccessHandler(JwtRepository jwtRepository) {
-        JwtService jwtService = new JwtService(jwtRepository);
-        return (request, response, authentication) -> {
-            jwtService.saveLogoutAccessToken(request.getHeader("Authorization"));
-            jwtService.deleteRefreshToken(JwtUtil.getLoginEmail(request.getHeader("Authorization")));
-        };
-    }
-
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
     }
 
 }
