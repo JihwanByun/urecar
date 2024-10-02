821544ce94aa910ed51a31ea60cd3c35728d089d
박동환 <znxn123@naver.com>
2024-10-02 17:14:21 +0900

    Feat: create dto about notification

---
+++ notification/src/main/java/com/ssafy/a303/notification/dto/NotificationSendToFcmServerDto.java
@@ -0,0 +1,15 @@
+package com.ssafy.a303.notification.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Builder
+@Getter
+public class NotificationSendToFcmServerDto {
+
+    private Long memberId;
+    private Long contentId;
+    private String title;
+    private String content;
+
+}
