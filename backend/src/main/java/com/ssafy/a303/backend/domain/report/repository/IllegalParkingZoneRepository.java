6ac800bb80418d89e444b581e0744c30df430fce
변지환 <jihwaja9091@naver.com>
2024-10-02 17:15:56 +0900

    Feat: add calculate distance from illegalparkingzone

--- backend/src/main/java/com/ssafy/a303/backend/domain/report/repository/IllegalParkingZoneRepository.java
+++ backend/src/main/java/com/ssafy/a303/backend/domain/report/repository/IllegalParkingZoneRepository.java
@@ -4,6 +4,7 @@ import com.ssafy.a303.backend.domain.report.entity.IllegalParkingZone;
 import com.ssafy.a303.backend.domain.report.entity.Location;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -11,6 +12,11 @@ import java.util.List;
 @Repository
 public interface IllegalParkingZoneRepository extends JpaRepository<IllegalParkingZone, Long> {
 
-    @Query("SELECT new com.ssafy.a303.backend.domain.report.entity.Location(i.location.longitude, i.location.latitude) FROM IllegalParkingZone i ")
-    List<Location> findAllLocation();
+//    @Query("SELECT new com.ssafy.a303.backend.domain.report.entity.Location(i.location.longitude, i.location.latitude) FROM IllegalParkingZone i ")
+
+    @Query("SELECT z FROM IllegalParkingZone z WHERE " +
+            "(6371000 * ACOS(COS(RADIANS(:latitude)) * COS(RADIANS(z.location.latitude)) * " +
+            "COS(RADIANS(z.location.longitude) - RADIANS(:longitude)) + " +
+            "SIN(RADIANS(:latitude)) * SIN(RADIANS(z.location.latitude)))) <= 20")
+    List<IllegalParkingZone> findWithin20Meters(@Param("longitude") double longitude, @Param("latitude") double latitude);
 }
