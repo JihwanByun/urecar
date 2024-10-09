3cda658100365d37d79c5a704ff7e026723bd6f4
박동환 <znxn123@naver.com>
2024-10-09 16:21:01 +0900

    Feat: create notification func

---
+++ notification/src/main/java/com/ssafy/a303/notification/service/ResultNotificationService.java
@@ -0,0 +1,18 @@
+package com.ssafy.a303.notification.service;
+
+import com.ssafy.a303.notification.dto.NotificationRequestDto;
+import com.ssafy.a303.notification.dto.NotificationResponseDto;
+import java.util.List;
+
+public interface ResultNotificationService {
+
+    void sendFirstNotification(NotificationRequestDto dto);
+
+    void sendSecondNotification(NotificationRequestDto dto);
+
+    List<NotificationResponseDto> getNotifications(long id);
+
+    void deleteByNotificationId(long id);
+
+    void deleteByMemberId(long id);
+}
