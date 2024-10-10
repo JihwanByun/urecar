61fd7029a5695d8309ef340795cadc1013aaf898
박동환 <znxn123@naver.com>
2024-10-10 16:39:24 +0900

    Feat: result notification

--- notification/src/main/java/com/ssafy/a303/notification/service/ResultNotificationService.java
+++ notification/src/main/java/com/ssafy/a303/notification/service/ResultNotificationService.java
@@ -15,4 +15,6 @@ public interface ResultNotificationService {
     void deleteByNotificationId(long id);
 
     void deleteByMemberId(long id);
+
+    void sendResultNotification(NotificationRequestDto dto);
 }
